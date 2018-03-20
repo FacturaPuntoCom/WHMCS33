ffa4d4095e0027160e684742d479674666e8a6b5
Francisco González <paco@lightcone.com>
2018-03-20 17:38:55 -0600

    fix logo configuracion

--- modules/addons/facturacom/facturacom.php
+++ modules/addons/facturacom/facturacom.php
@@ -27,7 +27,7 @@ function facturacom_config()
 {
     $configarray = [
         'name' => 'Factura.com',
-        'version' => '2.0',
+        'version' => '2.0.8',
         'author' => 'Factura.com',
         'logo' => '../modules/addons/facturacom/factura-addon-icon.png',
         'description' => 'Módulo de integración con el servicio de
